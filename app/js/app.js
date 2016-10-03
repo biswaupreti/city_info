f2e490704146b229aba9c37f06354b2e5a92aadc
Jarkko <jsaarinen92@gmail.com>
2016-10-03 15:53:22 +0300

    Binded mapdivs size to $rootScope.window_dimensions in html
    Moved map init to controller scope

--- app/js/app.js
+++ app/js/app.js
@@ -9,59 +9,13 @@ pw.run(['$rootScope', function($rootScope){
 
 	$rootScope.fullscreen = false;
 	$rootScope.initialized = false;
-  $rootScope.mapInitialized = false;
 
 	llb_app.addListener('window_state', function(data){
 		if(data.fullscreen)
 		{
 			$rootScope.$apply(function(){
 				$rootScope.fullscreen = true;
-        setMapSize($rootScope.window_dimensions.fullscreen_width, $rootScope.window_dimensions.fullscreen_height);
-
-        if (!$rootScope.mapInitialized) {
-
-          var myLatLng = new google.maps.LatLng(
-            61.497054, 23.758784
-          );
-
-          if ($rootScope.latestLocation) {
-            myLatLng = new google.maps.LatLng(
-              $rootScope.latestLocation.latitude,
-              $rootScope.latestLocation.longitude
-            );
-          }
-
-          var mapOptions = {
-            zoom: 14,
-            center: myLatLng,
-            mapTypeId: 'roadmap'
-          };
-          var mapDiv = document.getElementById('gmap');
-          var map = new google.maps.Map(mapDiv, mapOptions);
-
-          var placesService = new google.maps.places.PlacesService(map);
-          placesService.nearbySearch({
-            location: myLatLng,
-            radius: 500,
-            type: ['cafe']
-          }, function(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-              for (var i = 0; i < results.length; i++) {
-                place = results[i];
-                var placeLoc = place.geometry.location;
-                var marker = new google.maps.Marker({
-                  map: map,
-                  position: place.geometry.location
-                });
-              }
-            }
-          });
-
-          $rootScope.mapInitialized = true;
-        }
-
 			})
-
 		}
 		else
 		{
@@ -76,9 +30,7 @@ pw.run(['$rootScope', function($rootScope){
 		$rootScope.$apply(function(){
 			$rootScope.window_dimensions = data
 			$rootScope.initialized = true;
-
-      setMapSize(data.fullscreen_width, data.fullscreen_height);
-		})
+		});
 	})
 
   llb_app.request('location')
@@ -87,49 +39,43 @@ pw.run(['$rootScope', function($rootScope){
 	     $rootScope.latestLocation = data;
 		})
 	})
+}])
 
-  function setMapSize(windowWidth, windowHeight) {
-    var mapDiv = document.getElementById('gmap');
-    if (mapDiv != null) {
-      mapDiv.style.width = windowWidth + "px";
-      mapDiv.style.height = (windowHeight - 64) + "px"; //64px is height of the top thing with 'Living Lab Bus' text on it
+pw.controller('MainController', function($scope, $http){
+  $scope.mapInitialized = false;
+  $scope.$watch('fullscreen', function(newVal, oldVal) {
+    if (newVal == true && !$scope.mapInitialized) {
+      var myLatLng = new google.maps.LatLng(
+        61.497054, 23.758784
+      );
+
+      var mapOptions = {
+        zoom: 14,
+        center: myLatLng,
+        mapTypeId: 'roadmap'
+      };
+      var mapDiv = document.getElementById('gmap');
+      var map = new google.maps.Map(mapDiv, mapOptions);
+
+      var placesService = new google.maps.places.PlacesService(map);
+      placesService.nearbySearch({
+        location: myLatLng,
+        radius: 500,
+        type: ['cafe']
+      }, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            place = results[i];
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+            });
+          }
+        }
+      });
+
+      $scope.mapInitialized = true;
     }
-  }
-}])
-
-pw.controller('MainController', ['$rootScope', '$http', function($rootScope, $http){
-	var vm = this
-	vm.date = new Date()
-	vm.loading = true;
-
-	vm.getQuote = function(code)
-	{
-		switch(true)
-		{
-			case code == 800:
-				return 'Vai niin, aurinkokin pääti tulla esille.'
-			case [801, 802, 803, 804].indexOf(code) > 0:
-				return 'Mitä mukava sää...'
-			case [500, 501, 502, 503, 504 , 511, 520, 521, 522, 531].indexOf(code) > 0:
-				return 'Aijaa. Taas sadetta.'
-			default:
-				return 'Vai niin, aurinkokin pääti tulla esille.'
-		}
-	}
-
-	vm.getIcon = function(code)
-	{
-		switch(true)
-		{
-			case code == 800:
-				return 'img/sunny.gif'
-			case [801, 802, 803, 804].indexOf(code) > 0:
-				return 'img/cloudy.png'
-			case [500, 501, 502, 503, 504 , 511, 520, 521, 522, 531].indexOf(code) > 0:
-				return 'img/rainy.gif'
-			default:
-				return 'img/sunny.gif'
-		}
-
-	}
-}])
+  });
+});
