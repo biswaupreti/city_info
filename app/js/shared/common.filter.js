6da2a46436b691fd62c1a122434416786d7477a2
Jara <jsaarinen92@gmail.com>
2016-12-06 15:26:25 +0200

    Filter out 'point_of_interest' category
    99% of places have this category so it kinda loses it's meaning. Created
    a filter to do the job, strBlacklist can either take one string or an
    array for string to remove

--- app/js/shared/common.filter.js
+++ app/js/shared/common.filter.js
@@ -11,4 +11,34 @@
         }
       };
     })
+    .filter('strBlacklist', function () {
+      return function (input, blacklist) {
+        if (typeof blacklist === 'string') {
+          var blacklistStr = blacklist
+          blacklist = [blacklistStr];
+        }
+
+        if (typeof input === 'string') {
+          !checkContains(input, blacklist) ? input : null;
+        }
+        else if (input !== null && input !== undefined) {
+          var passedCategories = [];
+          for (var i = 0; i < input.length; ++i) {
+            var category = input[i];
+            if (!checkContains(category, blacklist)) {
+              passedCategories.push(category);
+            }
+          }
+
+          return passedCategories;
+        }
+        else {
+          return null;
+        }
+      };
+
+      function checkContains(needle, haystack) {
+        return haystack.indexOf(needle) > -1;
+      }
+    })
 })();
