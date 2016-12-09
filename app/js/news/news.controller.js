1e197c33f43462e96e84601c4ace824d315370bf
JunyiYang0925 <junyiyang0925@gmail.com>
2016-12-10 00:37:14 +0200

    Make news view works normally.(display and scroll)

--- app/js/news/news.controller.js
+++ app/js/news/news.controller.js
@@ -2,7 +2,7 @@
   'use strict';
   angular.module('cityInfo.news').controller('NewsController', function($http,$scope){
   	$scope.news= [];
-  	$http.get('https://newsapi.org/v1/articles?source=techcrunch&apiKey=4fd9c090e8c04c9f983076ed474461cd')
+  	$http.get('https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=4fd9c090e8c04c9f983076ed474461cd')
 		.success(function (response) {
 		    //console.log(response);
         angular.forEach(response.articles,function (news) {
